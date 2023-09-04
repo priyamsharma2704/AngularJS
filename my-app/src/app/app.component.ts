411c1eb0a47b71eb448b11ad59d9b58131883341
priyamsharma2704 <priyamsharma2704@gmail.com>
2023-09-04 00:03:11 -0400

    constructor and ngOnIt

--- my-app/src/app/app.component.ts
+++ my-app/src/app/app.component.ts
@@ -1,41 +1,48 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms'; // needed for ngModel
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'my-app';
 
+  constructor() {
+    alert('constructor: ' + this.title);
+  }
+
+  ngOnInit(): void {
+    alert('ngOnIt :' + this.title);
+  }
+
   //Tempalte Reference Var
-  handleClick(val:any)
-  {
+  handleClick(val: any) {
     console.log(val);
   }
- 
+
   //ngClass
   pokemons: Pokemon[] = [
     {
-      name:"pikachu",
+      name: 'pikachu',
       rank: 1,
       isActive: false,
-      type: "electric"
+      type: 'electric',
     },
     {
-      name:"charmander",
+      name: 'charmander',
       rank: 2,
       isActive: true,
-      type: "fire"
-    }
-  ]
+      type: 'fire',
+    },
+  ];
 }
 
 //ngClass
-interface Pokemon{
-  name:string,
-  rank: number,
-  isActive: boolean,
-  type: string
+interface Pokemon {
+  name: string;
+  rank: number;
+  isActive: boolean;
+  type: string;
 }
