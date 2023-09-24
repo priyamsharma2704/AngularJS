35e59f9726f1a96a7a4c9ce7aba41b293ecb2283
priyamsharma2704 <priyamsharma2704@gmail.com>
2023-09-24 00:16:04 -0400

    searching and getting the coin deytails

--- CryptoCurrency/src/app/app.component.ts
+++ CryptoCurrency/src/app/app.component.ts
@@ -8,11 +8,44 @@ import { CommonService} from './common.service';
 })
 export class AppComponent 
 {
-  title = 'CryptoCurrency';
+    title = 'CryptoCurrency';
+    coin:string = "";
+    coins: any = [];
 
-  constructor(private service: CommonService){
-    this.service.getCoins().subscribe(data=>{
-      console.log(data);
-    });
-  }
+    constructor(private service: CommonService)
+    {
+        this.service.getCoins().subscribe(data=>{
+            console.log("coins = " , data);
+            this.coins = data;
+        });
+
+        this.getCoin("razxDUgYGNAdQ");
+    }
+
+    getCoin(uuid: string)
+    {
+        console.log(uuid);
+        this.service.getCoin(uuid).subscribe(coin=>{
+            console.log("Coin = " , coin);
+        });
+    }
+
+    onInputChange(event:any)
+    {
+        this.coin = event.target.value;
+    }
+
+    getCoinDetails()
+    {
+        console.log("get coin details");
+        var coins = this.coins.data.coins;
+        for(var i = 0 ; i <coins.length; i++)
+        {
+            if(this.coin == coins[i].name.toLowerCase())
+            {
+                this.getCoin(coins[i].uuid);
+                break;
+            }
+        }
+    }
 }
--- CryptoCurrency/src/app/common.service.ts
+++ CryptoCurrency/src/app/common.service.ts
@@ -7,17 +7,40 @@ import { HttpClient } from '@angular/common/http';
 export class CommonService {
   constructor(private http: HttpClient) {}
 
-  getCoins() {
-    const url =
-      'https://coinranking1.p.rapidapi.com/coins?referenceCurrencyUuid=yhjMzLPhuIDl&timePeriod=24h&tiers%5B0%5D=1&orderBy=marketCap&orderDirection=desc&limit=50&offset=0';
-    const options = {
-      method: 'GET',
-      headers: {
-        'X-RapidAPI-Key': '7f3b62a5aamshfd589dde56ad001p1bf625jsnf97a213f15b5',
-        'X-RapidAPI-Host': 'coinranking1.p.rapidapi.com',
-      },
-    };
+    getCoins() 
+    {
+            const url =
+            'https://coinranking1.p.rapidapi.com/coins?referenceCurrencyUuid=yhjMzLPhuIDl&timePeriod=24h&tiers%5B0%5D=1&orderBy=marketCap&orderDirection=desc&limit=50&offset=0';
+            const options = {
+            method: 'GET',
+            headers: {
+                'X-RapidAPI-Key': '7f3b62a5aamshfd589dde56ad001p1bf625jsnf97a213f15b5',
+                'X-RapidAPI-Host': 'coinranking1.p.rapidapi.com',
+            },
+            };
 
-    return this.http.get(url, options);
-  }
+            return this.http.get(url, options);
+    }
+
+    getCoin(uuid: string)
+    {
+        console.log("in service, uuid:" + uuid);
+        const options = {
+            method: 'GET',
+            url: 'https://coinranking1.p.rapidapi.com/coin/' + uuid,
+            params: {
+              referenceCurrencyUuid: 'yhjMzLPhuIDl',
+              timePeriod: '24h'
+            },
+            headers: {
+              'X-RapidAPI-Key': '7f3b62a5aamshfd589dde56ad001p1bf625jsnf97a213f15b5',
+              'X-RapidAPI-Host': 'coinranking1.p.rapidapi.com'
+            }
+          };
+
+        return this.http.get(options["url"], options);
+    }
+
+
+
 }
